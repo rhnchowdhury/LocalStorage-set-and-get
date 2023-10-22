2e12521f4939754a703eef9fe0ac803cda1f0cc3
Raihan <rhnchdri@gmail.com>
2023-10-22 18:38:11 +0600

    data add

--- src/utility/FakeDb.js
+++ src/utility/FakeDb.js
@@ -1,14 +1,34 @@
 // Use local storage to manage cart data
 
+////////////////////////////// Single data stored///////////////////////////////////////////
+
+// const addToDb = id => {
+//     const quantity = localStorage.getItem(id);
+//     if (quantity) {
+//         const newQuantity = parseInt(quantity) + 1;
+//         localStorage.setItem(id, newQuantity);
+//     }
+//     else {
+
+//         localStorage.setItem(id, 1)
+//     }
+// };
+
+
+////////////////////////////// Multiple data stored///////////////////////////////////////////
+
 const addToDb = id => {
-    const quantity = localStorage.getItem(id);
+    let shoppingCart = {};
+
+    const quantity = shoppingCart[id];
     if (quantity) {
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
     }
     else {
-
-        localStorage.setItem(id, 1)
+        shoppingCart[id] = 1;
     }
-}
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+};
+
 export default addToDb;
